85e1ae65edd0c2da7d1a82518db5a5091ac7cadc
jhha <hjhdog1@gmail.com>
2022-11-25 14:10:34 +0900

    constant workspace code supports acting point other than link origin.

--- forces/constGlobalLoad.m
+++ forces/constGlobalLoad.m
@@ -1,4 +1,9 @@
-function [f, df] = constGlobalLoad(T, f_global)
+function [f, df] = constGlobalLoad(T, f_global, p_acting)
+% p_acting: body-fixed point where the force is applied
+
+    if (nargin < 3)
+        p_acting = [0;0];
+    end
 
     R_trans = T(1:2, 1:2)';
 
@@ -8,6 +13,7 @@ function [f, df] = constGlobalLoad(T, f_global)
     if nargout > 1
         df = zeros(3);
         df(2:3,1) = R_trans * [f_global(3); -f_global(2)];
+        df(1,1) = -[p_acting(2), -p_acting(1)] * df(2:3,1);
     end
 
 end
